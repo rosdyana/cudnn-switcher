b2b4f36c5f7ec40d10792246194561d71fc7ccd7
rosdyana <rosdyana.kusuma@gmail.com>
2018-04-11 23:01:08 +0800

    remove sudoer

--- cudnnswitch.py
+++ cudnnswitch.py
@@ -2,28 +2,27 @@ import argparse
 import glob
 import subprocess
 import webbrowser
+import os
 
 
-def switchCudnn(version, password):
+def switchCudnn(version,):
     packages = glob.glob("packages/*{}.tgz".format(version))
     lenfiles = len(packages)
     if lenfiles:
         print("Switching cudnn version {}".format(version))
-        subprocess.Popen('sudo -s', shell=True, stdout=subprocess.PIPE)
-        subprocess.Popen(password, shell=True, stdout=subprocess.PIPE)
         subprocess.call(['mkdir', 'packages/cudnn'])
         subprocess.call(['tar', 'xf', packages[0],
                          '-C', 'packages/cudnn', '--strip-components', '1'])
         subprocess.call(['sudo rm -rf', '/usr/include/cudnn.h'])
         subprocess.call(
-            ['sudo rm -rf', '/usr/lib/x86_64-linux-gnu/*libcudnn*'])
-        subprocess.call(['sudo rm -rf', '/usr/local/cuda-*/lib64/*libcudnn*'])
+            ['rm -rf', '/usr/lib/x86_64-linux-gnu/*libcudnn*'])
+        subprocess.call(['rm -rf', '/usr/local/cuda-*/lib64/*libcudnn*'])
         subprocess.call(
-            ['sudo cp -P', 'packages/cudnn/include/cudnn.h /usr/include'])
+            ['cp -P', 'packages/cudnn/include/cudnn.h /usr/include'])
         subprocess.call(
-            ['sudo cp -P', 'packages/cudnn/lib64/libcudnn* /usr/lib/x86_64-linux-gnu/'])
+            ['cp -P', 'packages/cudnn/lib64/libcudnn* /usr/lib/x86_64-linux-gnu/'])
         subprocess.call(
-            ['sudo chmod a+r', '/usr/lib/x86_64-linux-gnu/libcudnn*'])
+            ['chmod a+r', '/usr/lib/x86_64-linux-gnu/libcudnn*'])
         subprocess.call(['rm -rf', 'packages/cudnn'])
         print("Sucessfully switched to version {}".format(version))
     else:
@@ -37,10 +36,8 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-v', '--version',
                         help='select cudnn version ( 4.0, 5.1, 6.0', required=True)
-    parser.add_argument('-p', '--password',
-                        help="sudo password", required=True)
     args = parser.parse_args()
-    switchCudnn(args.version, args.password)
+    switchCudnn(args.version)
 
 
 if __name__ == '__main__':
