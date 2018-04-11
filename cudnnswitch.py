d0f6ded1c8f3d47cda0bc70e979bcbb2880a85fc
rosdyana <rosdyana.kusuma@gmail.com>
2018-04-11 22:24:23 +0800

    initial commit

---
+++ cudnnswitch.py
@@ -0,0 +1,41 @@
+import argparse
+import glob
+import subprocess
+import webbrowser
+
+
+def switchCudnn(version):
+    packages = glob.glob("packages/*v{}.tgz".format(version))
+    lenfiles = len(packages)
+    if lenfiles:
+        print("Switching cudnn version {}".format(version))
+        subprocess.call(['mkdir', 'packages/cudnn'])
+        subprocess.call(['tar', 'xf', packages[0],
+                         '-C', 'packages/cudnn', '--strip-components', '1'])
+        subprocess.call(['rm -f', '/usr/include/cudnn.h'])
+        subprocess.call(['rm -f', '/usr/lib/x86_64-linux-gnu/*libcudnn*'])
+        subprocess.call(['rm -f', '/usr/local/cuda-*/lib64/*libcudnn*'])
+        subprocess.call(
+            ['cp -P', 'packages/cudnn/include/cudnn.h /usr/include'])
+        subprocess.call(
+            ['cp -P', 'packages/cudnn/lib64/libcudnn* /usr/lib/x86_64-linux-gnu/'])
+        subprocess.call(['chmod a+r', '/usr/lib/x86_64-linux-gnu/libcudnn*'])
+        subprocess.call(['rm -rf', 'packages/cudnn'])
+        print("Sucessfully switched to version {}".format(version))
+    else:
+        print("Please download cudnn first")
+        webbrowser.open_new_tab(
+            "https://developer.nvidia.com/rdp/cudnn-archive")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-v', '--version',
+                        help='select cudnn version ( 4.0, 5.1, 6.0', required=True)
+    args = parser.parse_args()
+    switchCudnn(args.version)
+
+
+if __name__ == '__main__':
+    main()
