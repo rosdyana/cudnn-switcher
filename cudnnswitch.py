a2e6b71ffe097d57d604651cfa6e8d2d09a03bd1
rosdyana <rosdyana.kusuma@gmail.com>
2018-04-11 23:29:05 +0800

    handle run process

--- cudnnswitch.py
+++ cudnnswitch.py
@@ -5,22 +5,29 @@ import webbrowser
 import os
 
 
+def runprocess(command):
+    try:
+        subprocess.call(command, shell=True)
+    except Exception as e:
+        print(e)
+
+
 def switchCudnn(version, cudadir):
     packages = glob.glob("packages/*{}.tgz".format(version))
     lenfiles = len(packages)
     if lenfiles:
         print("Switching cudnn version {}".format(version))
-        subprocess.call(['mkdir', 'packages/cudnn'])
-        subprocess.call(['tar', 'xf', packages[0],
-                         '-C', 'packages/cudnn', '--strip-components', '1'])
-        subprocess.call(['rm -rf', '{}/include/cudnn.h'.format(cudadir)])
-        subprocess.call(['rm -rf', '{}/lib64/*libcudnn*'.format(cudadir)])
-        subprocess.call(
-            ['cp -P', 'packages/cudnn/include/cudnn.h {}/include'.format(cudadir)])
-        subprocess.call(
-            ['cp -P', 'packages/cudnn/lib64/libcudnn* {}/lib64'.format(cudadir)])
-        subprocess.call(
-            ['chmod a+r', '{}/lib64/libcudnn*'.format(cudadir)])
+        runprocess('mkdir packages/cudnn')
+        runprocess(
+            'tar xf {} -C packages/cudnn --strip-components 1'.format(packages[0]))
+        runprocess('sudo rm -rf {}/include/cudnn.h'.format(cudadir))
+        runprocess('sudo rm -rf {}/lib64/*libcudnn*'.format(cudadir))
+        runprocess(
+            'sudo cp -P packages/cudnn/include/cudnn.h {}/include'.format(cudadir))
+        runprocess(
+            'sudo cp -P packages/cudnn/lib64/libcudnn* {}/lib64'.format(cudadir))
+        runprocess(
+            'sudo chmod a+r {}/include/cudnn.h {}/lib64/libcudnn*'.format(cudadir, cudadir))
         subprocess.call(['rm -rf', 'packages/cudnn'])
         print("Sucessfully switched to version {}".format(version))
     else:
